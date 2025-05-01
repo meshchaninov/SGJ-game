268c8e363a031a79d32394b69fc106faa570666e
Grigoriy Kustov <grishakustov@yandex.ru>
2025-05-02 02:27:03 +0700

    Added global state

---
+++ globalState.gd
@@ -0,0 +1,5 @@
+extends Node
+
+# Вот тут описываем все состояния, которые переносятся между сценами
+# https://forum.godotengine.org/t/transfering-a-variable-over-to-another-scene/33407/5
+# Это синглтон, по сути. Обращаться так: GlobalState.field
